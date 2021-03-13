b0934698a515ea7d4c938352e8e8dfc9d2086a69
laya-star <78084935+laya-star@users.noreply.github.com>
2021-03-13 10:00:48 +0530

    Add files via upload

--- music.js
+++ music.js
@@ -3,6 +3,7 @@ leftWristX=0
 leftWristY=0
 rightWristX=0
 rightWristY=0
+scoreLeftWrist=0
 function preload(){
     song=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -18,19 +19,29 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500);
+    fill('#dffc03');
+    stroke('#dffc03');
 }
 function modelLoaded(){
     console.log("PoseNet is Initialized");
 }
-function gotPoses(){
+function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist="+scoreLeftWrist);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
-        console.log("leftWristX="+leftWrist+"leftWristY="+leftWristY);
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
 
         righttWristX=results[0].pose.rightWrist.x;
         rightWristY=results[0].pose.rightWrist.y;
-        console.log("rightWristX="+rightWrist+"rightWristY="+rightWristY);
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
     }
 }
+function song_name(){
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX, leftWristY,20);
+        song1.play();
+        }
+    }
